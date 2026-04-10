d27596fa7000a1a44308ae89c67c3a3209afcccf
kangminhun <96628581+kangminhun@users.noreply.github.com>
2026-04-10 14:37:01 +0900

    두번째 커밋

---
+++ src/main/java/com/example/sbb/question/QuestionService.java
@@ -0,0 +1,30 @@
+package com.example.sbb.question;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+import java.util.Optional;
+import com.example.sbb.DataNotFoundException;
+
+@RequiredArgsConstructor
+@Service
+public class QuestionService {
+
+    private final QuestionRepository questionRepository;
+
+    public List<Question> getList() {
+        return this.questionRepository.findAll();
+    }
+    
+    public Question getQuestion(Integer id) {  
+        Optional<Question> question = this.questionRepository.findById(id);
+        if (question.isPresent()) {
+            return question.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
+    }
+}
